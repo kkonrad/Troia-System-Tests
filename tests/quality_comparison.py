8cceb37b6fa90b259dfb10d071fdb52ea1672e80
Piotr Gnys <piotr.gnys@10clouds.com>
2012-11-06 10:06:59 +0100

    Fixed python errors

--- tests/quality_comparison.py
+++ tests/quality_comparison.py
@@ -1,6 +1,6 @@
 import time
 import sys
-sys.path.append('..')
+sys.path.append('.')
 
 
 from tools.loading_data import load_all
@@ -10,14 +10,23 @@ from tools.troia import get_troia_client
 DATASETS = ['datasets/' + s for s in ('AdultContent', 'BarzanMozafari')]
 
 def executeDawidSkene(jid, tc, iterations, incremental, golds, cost_matrix, assigns):
+    print "Executing DS"
     tc.ping()
+    print "Ping succesfull"
     if tc.exists(jid):
         tc.reset(jid)
+        print "DS model reset"
+    else:
+        print "No need for DS reset"
     tc.load_categories(cost_matrix, jid)
+    print "DS model created"
     tc.load_worker_assigned_labels(assigns, jid)
+    print "Labels loaded"
     tc.load_gold_labels(golds, jid)
+    print "Gold labels loaded"
     time.sleep(4)
     tc.compute_non_blocking(iterations, jid)
+    print "Created computer"
     while 'true' not in tc.is_computed(jid):
         time.sleep(2)
     return tc.majority_votes(jid)
@@ -25,15 +34,16 @@ def executeDawidSkene(jid, tc, iterations, incremental, golds, cost_matrix, assi
 
 class QualityComparisionTest():
 
-    def init(self,tc,jid,iterations,dataset):
+    def __init__(self,tc,jid,iterations,datasets):
         self.jid = jid
         self.tc=tc
-        self.dataset=dataset
+        self.datasets=datasets
         self.iterations = iterations
 
 
     def run(self):
         print "Begining quality comparition test"
+        dataset = self.datasets[1]
         incrementalResults = executeDawidSkene(self.jid,self.tc,self.iterations,True,*dataset)
         batchResults = executeDawidSkene(self.jid,self.tc,self.iterations,False,*dataset)
         labelCount = 0
@@ -44,4 +54,9 @@ class QualityComparisionTest():
         print "Differing labels = " + differingMajorityVoteCount
 
 def main():
-    datastes = [load_all(ds) for ds in DATASETS]
+    datasets = [load_all(ds) for ds in DATASETS]
+    test = QualityComparisionTest(get_troia_client(),"QualityTest",10,datasets)
+    test.run()
+
+if __name__ == "__main__":
+    main()
