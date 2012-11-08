89c4696615de200b762891ef70ced0674b5fc190
Piotr Gnyś <drregule@gmail.com>
2012-11-08 11:00:07 +0100

    Fix

--- tools/loading_data.py
+++ tools/loading_data.py
@@ -3,14 +3,16 @@ import os
 
 from tools.common import transform_cost_from_flat_to_dict
 
+def read_data_file(pathh):
+    pathh = os.path.normpath(pathh)
+    return list(csv.reader(open(pathh), delimiter='\t'))
 
 def load_all(path):
-    def tmp(pathh):
-        pathh = os.path.normpath(pathh)
-        return list(csv.reader(open(pathh), delimiter='\t'))
-
     correct, costs, inputt = \
-        [tmp(path + s) for s in ['/correct', '/costs', '/input']]
+        [read_data_file(path + s) for s in ['/correct', '/costs', '/input']]
     correct = [x[-2:] for x in correct]
     costs = transform_cost_from_flat_to_dict(costs)
     return correct, costs, inputt
+
+
+
